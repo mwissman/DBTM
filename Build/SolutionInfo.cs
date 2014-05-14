486caa98df1a487ff0fb366c215e3e00f336d910
Matt Wissman <mwissman@outlook.com>
2014-05-13 21:53:56 -0500

    Added File and Info Version

--- Build/SolutionInfo.cs
+++ Build/SolutionInfo.cs
@@ -1,5 +1,7 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.0.1")]
-[assembly: AssemblyCopyright("Copyright ©  2009-2014")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
+[assembly: AssemblyInformationalVersion("2.0")]
+[assembly: AssemblyCopyright("Copyright © 2009-2014")]
 [assembly: AssemblyProduct("Database Transition Manager")]
