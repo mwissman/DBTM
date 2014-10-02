2df6d47b9f517c59fd99145042d60f2db729068d
Matthew Wissman <mwissman@rewardsnetwork.com>
2014-10-02 16:27:49 -0500

    hacked together some migrators to make importing large scripts easier. disabled for now

--- Source/DBTM.Domain/DomainModule.cs
+++ Source/DBTM.Domain/DomainModule.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using DBTM.Domain.Entities;
 
 namespace DBTM.Domain
 {
@@ -20,9 +21,32 @@ namespace DBTM.Domain
                 .As<IGuidFactory>()
                 .InstancePerLifetimeScope();
 
+//            builder.Register(c => new CombinedMigrator(new EnsureStatementsHaveIds(c.Resolve<IGuidFactory>()),new GenerateDescriptions(),new RemoveAnsiNullStatements()))
             builder.Register(c => new EnsureStatementsHaveIds(c.Resolve<IGuidFactory>()))
                 .As<IMigrator>()
                 .InstancePerLifetimeScope();
+        }
+    }
+
+    public class CombinedMigrator : IMigrator
+    {
+        private readonly EnsureStatementsHaveIds _ensureStatementsHaveIds;
+        private readonly GenerateDescriptions _generateDescriptions;
+        private readonly RemoveAnsiNullStatements _removeAnsiNullStatements;
+
+        public CombinedMigrator(EnsureStatementsHaveIds ensureStatementsHaveIds, GenerateDescriptions generateDescriptions, RemoveAnsiNullStatements removeAnsiNullStatements)
+        {
+            _ensureStatementsHaveIds = ensureStatementsHaveIds;
+            _generateDescriptions = generateDescriptions;
+            _removeAnsiNullStatements = removeAnsiNullStatements;
+        }
+
+
+        public void Migrate(Database database)
+        {
+            _ensureStatementsHaveIds.Migrate(database);
+            _generateDescriptions.Migrate(database);
+            _removeAnsiNullStatements.Migrate(database);
         }
     }
 }
--- Source/DBTM.Domain/EnsureStatementsHaveIds.cs
+++ Source/DBTM.Domain/EnsureStatementsHaveIds.cs
@@ -28,4 +28,57 @@ namespace DBTM.Domain
             sqlStatementCollection.Where(s=>s.Id==  Guid.Empty).ForEach(s=>s.Id=_guidFactory.Create());
         }
     }
+
+    public class GenerateDescriptions : IMigrator
+    {
+        public void Migrate(Database database)
+        {
+            database.Versions.ForEach(v =>
+            {
+                GenerateDescription(v.PreDeploymentStatements);
+                GenerateDescription(v.PostDeploymentStatements);
+            });
+        }
+
+        private void GenerateDescription(SqlStatementCollection sqlStatementCollection)
+        {
+           sqlStatementCollection.Where(s=>s.Description=="---").ForEach(s =>
+           {
+               var lines=s.UpgradeSQL.Split(new[] {"\r\n", "\n"}, StringSplitOptions.RemoveEmptyEntries);
+
+               if (lines.Length > 0)
+               {
+                   s.Description = lines[0].Truncate(50);
+               }
+
+           });
+        }
+    }
+
+    public class RemoveAnsiNullStatements : IMigrator
+    {
+        public void Migrate(Database database)
+        {
+            database.Versions.ForEach(v =>
+            {
+                RemoveAnsiNullStatement(v.PreDeploymentStatements);
+                RemoveAnsiNullStatement(v.PostDeploymentStatements);
+            });
+        }
+
+        private void RemoveAnsiNullStatement(SqlStatementCollection sqlStatementCollection)
+        {
+            var toRemove=sqlStatementCollection.Where(s => s.UpgradeSQL.Contains("SET ANSI_NULLS ON")).ToList();
+            toRemove.ForEach(sqlStatementCollection.Remove);
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string Truncate(this string value, int maxLength)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+        }
+    }
 }
